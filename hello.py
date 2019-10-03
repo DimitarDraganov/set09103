224ef47dad223df87438dfde21c9d2b1036bdd21
Dimitar Draganov <40284378@live.napier.ac.uk>
2019-10-03 09:56:33 +0100

    Url redirect working

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +8,16 @@ def hello():
 @app.route("/goodbye/")
 def goodbye():
     return "Goodbye cruel world  :C"
+
+@app.route("/private")
+def private():
+    #Test for user logged in failed
+    #so redicted to login URL
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return "Now we would get username & password"
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
