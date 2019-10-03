134035b6d20ae0a18e4f8eb515391d7e611903dd
Dimitar Draganov <40284378@live.napier.ac.uk>
2019-10-03 10:35:56 +0100

    Using an image

--- hello.py
+++ hello.py
@@ -27,6 +27,12 @@ def force404():
 def page_not_found(error):
     return "Couldnt find the PAGE you requested", 404
 
+@app.route('/static-example/img')
+def static_example_img():
+    start = '<img src="'
+    url = url_for('static', filename='vmask.jpg')
+    end = '">'
+    return start+url+end, 200
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
