0d1b2b19a59f3023c4c9891a6bd2db2cf2f2226e
Dimitar Draganov <40284378@live.napier.ac.uk>
2019-10-24 10:21:37 +0100

    finishing week 6

--- exWeek6.py
+++ exWeek6.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/hello/')
@@ -6,6 +6,32 @@ app = Flask(__name__, template_folder='templates')
 def hello(name=None):
     return render_template('conditional.html', name=name)
 
+@app.route('/users/')
+def users():
+    names = ['simon', 'Ivan', 'Gina', 'Mitko', 'Alex']
+    return render_template('loops.html', names=names)
+
+
+@app.route("/account/", methods=['POST','GET'])
+def account():
+    if request.method == 'POST':
+        print request.form
+        name = request.form['name']
+        return "Hello %s" % name
+    else:
+        page ='''
+        <html><body>
+            <form action="" method="post" name="form">
+            <label for="name">Name:</label>
+            <input type="text" name="name" id="name"/>
+            <input type="submit" name="submit" id="submit"/>
+            </form>
+            </body><html>'''
+
+        return page
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+
 
